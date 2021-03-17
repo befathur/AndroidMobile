c21c19c2362c5dfd9bb8dbc4bf2a16ac29b70c5c
befathur <kiagus.fathur@student.umn.ac.id>
2021-03-17 14:59:01 +0700

    IniTugas

---
+++ Week04A_33479/app/src/main/java/umn/ac/id/week04a_33479/SecondActivity.java
@@ -0,0 +1,15 @@
+package umn.ac.id.week04a_33479;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        setTitle("Week04_33479");
+    }
+}
